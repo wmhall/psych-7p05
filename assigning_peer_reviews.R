497a1edb30e3a8b714beb6407958bdae281df830
Will Hall <will.hall@utoronto.ca>
2024-07-25 13:44:56 -0400

    code for assigning peer reviews

---
+++ assigning_peer_reviews.R
@@ -0,0 +1,97 @@
+library(tidyverse)
+library(gt)
+
+set.seed(1985)
+
+class_list <- 
+read_csv("working/peer-review-assignments/class-list.csv")
+
+student_names <- 
+class_list |> 
+	select(first_name = `First Name`, last_name = `Last Name`) |> 
+	mutate(name = glue::glue("{first_name} {last_name}")) |> 
+	select(name)
+
+assign_reviews <- function(name_of_rev, n) {
+	student_names |> 
+		filter(!name %in% name_of_rev) |> 
+		sample_n(n) |> 
+		pull(name)
+}
+
+get_sample <- function() {
+	success <- FALSE
+	while (!success) {
+		# do something
+		x <- student_names |> 
+			mutate(peer_rev = map(name, ~assign_reviews(., 15))) |> 
+			unnest(peer_rev) |> 
+			group_by(name) |> 
+			mutate(n = row_number(), 
+						 hw = cut(n, 5) |> as.numeric(), 
+						 hw_n = paste0("hw", hw))
+		
+		y <- 
+			x |> 
+			group_by(peer_rev, hw_n) |> 
+			count() |>
+			group_by(peer_rev) |> 
+			count() |> 
+			filter(n != 5)
+		# check for success
+		success <- nrow(y) == 0
+	}
+	return(x)
+}
+
+assigned_revs <- 
+	get_sample() |> 
+	group_by(name, hw_n) |> 
+	mutate(n = row_number(), 
+				 peer_rev_n = paste0("peer_", n)) |> 
+	select(!c(n, hw)) |> 
+	group_by(hw_n) |> 
+	nest() |> 
+	mutate(table = map(data, ~pivot_wider(., names_from = peer_rev_n, 
+																				values_from = peer_rev) |> 
+										 	arrange(name)))
+
+
+make_table <- function(data) {
+	data |> 
+		gt() |>
+		cols_label(
+			name = md("**Name**"),
+			peer_1 = md("**Assigned Review 1**"),
+			peer_2 = md("**Assigned Review 2**"),
+			peer_3 = md("**Assigned Review 3**")) |> 
+		cols_align(
+			align = c("left"),
+			columns = everything()
+			)
+}
+
+gts$gt_table[[1]]
+
+gts <- 
+assigned_revs |> 
+	mutate(gt_table = map(table, ~make_table(.)))
+
+glue::glue("working/peer-review-assignments/{gts$hw_n}.html") |> 
+	walk2(.x = gts$gt_table, .y = _, ~gtsave(.x, .y))
+
+# checking review counts --------------------------------------------------
+
+
+gts |> 
+	select(hw_n, data) |> 
+	unnest(data) |> 
+	select(-name) |> 
+	group_by(peer_rev, hw_n) |> 
+	count() |>
+	group_by(peer_rev) |> 
+	count() |> 
+	filter(n != 5)
+
+
+
